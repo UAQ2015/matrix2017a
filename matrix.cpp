d7dae31cab9778eb6bc3a65763d8c23319b2dfb1
adri <adriana.ga27@live.com>
2017-05-11 13:09:21 -0500

    Read matrix and print matrix

--- matrix.cpp
+++ matrix.cpp
@@ -1,10 +1,45 @@
 #include "matrix.h"
-//#include <iostream.h>
+#include <iostream>
+
+using namespace std;
 
 void Matrix::Read()
-{  
+{	cout<<"Por favor introduzca el numero de renglones\n";  
+	cin>> m;
+
+	cout<<"Por favor introduzca el numero de columnas\n";
+	cin>> n;
+
+	CreateArray();
+
+	for(int i=0;i<m; i++)
+		for(int j=0;j<n;j++)
+		{  cout<<"Introduce el dato arr["<<i<<"]["<<j<<"]\n";
+		   cin>>mat[i][j];
+		}
+}
+void Matrix::Print()
+{	cout<<"Imprimiendo matriz de "<< m <<" renglones y "<<n<<" columnas\n";	
+	for(int i=0;i<m;i++)
+	{   for(int j=0;j<n;j++)
+		{	cout<<mat[i][j]<<" ";
+		}	
+		cout<<endl;
+	}
 }
 
-void Matrix::Print()
-{
+
+void Matrix::CreateArray()
+{	mat= new float *[m];
+	for(int i=0;i<m;i++)
+	{	mat[i]= new float[n];
+	}	
+
+}
+void Matrix::DeleteArray ()
+{	for(int i=0;i<m;i++)
+	{	delete mat[i];
+	}
+	delete mat;
+	mat = NULL;
 }
--- matrix.h
+++ matrix.h
@@ -7,7 +7,11 @@ public:
 	void Read();
 	void Print();
 private:
-
+	int m; // Row number
+	int n; // Col number
+	float **mat;
+	void CreateArray();
+	void DeleteArray();
 };
 
 #endif
